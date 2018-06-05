210f50fb4307632a96b0c5c76bacb54a80ef220a
hence-dense <39894642+hence-dense@users.noreply.github.com>
2018-06-05 07:57:23 +0300

    Improve task 3

--- task3.rb
+++ task3.rb
@@ -1,8 +1,11 @@
+# Task 3
+
 def char_filter(str)
   a = str.scan(/[aeiou]/i).join('')
   b = str.scan(/[^aeiou0-9\W]/i).join('')
   c = str.scan(/[0-9]/).join('')
-  puts "#{a} #{b} #{c}"
+
+  puts "#{a} #{b} #{c}".strip
 end
 
 char_filter("#{ARGV[0]}")
