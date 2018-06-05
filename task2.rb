b680c180b0bd2b09af511b52f5a7d7bda995baa2
hence-dense <39894642+hence-dense@users.noreply.github.com>
2018-06-05 11:42:04 +0300

    Improve task 2 solution

--- task2.rb
+++ task2.rb
@@ -1,27 +1,37 @@
 # Task 2
 
 # Functions
-# Add negative sequence handling
 def fib(input)
   # check input string numericness
   tester = Integer(input) != nil rescue false
   n = input.to_i if tester
 
   exit if !( n.kind_of?(0.class) )
-  # exit if (n < -10000) || (n > 10000)
-  exit if (n < 0) || (n > 10000)
+  exit if (n < -10000) || (n > 10000)
 
   return 0 if (n == 0)
-  return 1 if (n == 1)
+  return 1 if (n == 1) || (n == -1)
 
   if n > 1
     i = 0
     j = 1
-    0.upto(n-2).each do
+    0.upto(n-2) do
       buffer = i + j
       i = j
       j = buffer
     end
+    return j
+  end
+
+  if (n.negative?)
+    i = 0
+    j = 1
+    0.upto(n.abs-2) do
+      buffer = i + j
+      i = j
+      j = buffer
+    end
+    j = j*(-1) if n.even?
     return j
   end
 end
