ffa573756840be57da2eb0c7363adfea1ae3dfaf
hence-dense <39894642+hence-dense@users.noreply.github.com>
2018-06-09 00:07:43 +0300

    Improve tasks solutions.

--- task3.rb
+++ task3.rb
@@ -4,11 +4,11 @@ def char_divider(input)
   exit if ((input.length < 1) || (input.length > 10000))
   exit if !input.ascii_only?
 
-  a = input.scan(/[aeiou]/i).join('')
-  b = input.scan(/[^aeiou0-9_\W]/i).join('')
+  a = input.scan(/[aeiouy]/i).join('')
+  b = input.scan(/[^aeiouy0-9_\W]/i).join('')
   c = input.scan(/[0-9]/).join('')
 
-  puts "#{a} #{b} #{c}".strip
+  puts "#{a} #{b} #{c}"
 end
 
 char_divider("#{ARGV[0]}")
