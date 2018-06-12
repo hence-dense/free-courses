128a74b9de3b0d6a97a96c4ef645781e9a504e99
hence-dense <39894642+hence-dense@users.noreply.github.com>
2018-06-13 00:32:26 +0300

    Testing.

--- task2.rb
+++ task2.rb
@@ -1,28 +1,10 @@
 # Task 2
+require 'matrix'
 
-# Functions
-def fib(input)
-  #return " " if ( input.nil? || !input.ascii_only? )
-  # check input string numericness
-  #n = input.to_i if ( Integer(input) rescue false )
-  n = input.to_i
-  #return " " if ( !( n.kind_of?(Integer) ) || (n < -10000) || (n > 10000) )
+def fibcalc(input)
+  m = Matrix[[0, 1], [1,1]]
 
-  return 0 if (n == 0)
-  return 1 if (n == 1) || (n == -1)
-
-  i = 0
-  j = 1
-  0.upto(n.abs-2) do
-    buffer = i + j
-    i = j
-    j = buffer
-  end
-  j = j*(-1) if n.even? && n.negative?
-  return j
+  result = (m**(input.to_i-1))[1,1].to_i
 end
 
-# Main
-ARGV.each do |item|
-  puts fib(item)
-end
+puts "#{fibcalc(ARGV[0])}"
