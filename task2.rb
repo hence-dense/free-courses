50d48290953f638f5dae69b5e7cf841602abad58
hence-dense <39894642+hence-dense@users.noreply.github.com>
2018-06-14 06:51:21 +0300

    Try, try, try, try, try, try, try, try, try
    and try again.

--- task2.rb
+++ task2.rb
@@ -1,10 +1,25 @@
 # Task 2
-require 'matrix'
 
-def fibcalc(input)
-  m = Matrix[[0, 1], [1,1]]
+def fib(input)
+  return " " if ( input.nil? || !input.ascii_only? || (input.match?(/[.,]/i)) )
+  num = input.to_i if ( Integer(input) rescue false )
+  return " " if ( !( num.kind_of?(Integer) ) || (num < -10000 || num > 10000) )
 
-  result = (m**(input.to_i-1))[1,1].to_i
+  return 0 if (num == 0)
+  return 1 if (num == 1) || (num == -1)
+
+  first = 0
+  last = 1
+  0.upto(num.abs-2) do
+    buf = first + last
+    first = last
+    last = buf
+  end
+  last = last*(-1) if num.even? && num.negative?
+  return last
 end
 
-puts "#{fibcalc(ARGV[0])}"
+# Main
+ARGV.each do |item|
+  puts fib(item)
+end
